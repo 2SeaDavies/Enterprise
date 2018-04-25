9dcf36c8796827483c067a9fd0d0f74e672935b6
Jackingotn <crdavies@hotmail.co.uk>
2018-04-25 18:16:53 +0100

    units die

--- battle.php
+++ battle.php
@@ -61,21 +61,64 @@ $damage2 = $strength2;
 $damage = $strength;
 
 
-$sql = "Select * from Unit,Player_Units where Unit.Unit_ID = Player_Units.Unit_ID and Player_Units.Name = 'Creg' and Ran in (Select min(Ran) from Unit,Player_Units where Unit.Unit_ID = Player_Units.Unit_ID and Num > 0 and Player_Units.Name = '$nom')";
+$sql = "Select * from Unit,Player_Units where Unit.Unit_ID = Player_Units.Unit_ID and Player_Units.Name = '$nom' and Ran in (Select min(Ran) from Unit,Player_Units where Unit.Unit_ID = Player_Units.Unit_ID and Num > 0 and Player_Units.Name = '$nom')";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
         echo "id: " . $row["Name"] . " - Name: " . $row["Unit_ID"] . " " . $row["Num"] . "<br>";
+        echo "<p>healthy $healthy </p>";
+        $HP = $row["HP"];
         $healthy+= $row["HP"];
+        $num = $row["Num"];
 
+        echo "<p>healthy $healthy </p>";
+        $ID = $row["Unit_ID"];
 
+        echo "<p>healthy $healthy </p>";
         $healthy *= $row["Num"];
+        echo "<p>healthy $healthy </p>";
+
 
 
         if ($damage2 > $healthy) {
 
+            $sql = "UPDATE Player_Units SET Num = 0 where Unit_ID = $ID and Player_Units.Name = '$nom'";
+
+            if ($conn->query($sql) === TRUE) {
+                $damage2 -= $healthy;
+                echo "<p> they died </p>";
+                echo "<p> $damage2 </p>";
+
+            } else {
+                echo "Error updating record: " . $conn->error;
+            }
+
+
             
+        }
+        else {
+            echo "<p>damage before: $damage2 </p>";
+            echo "<p>healthy $healthy </p>";
+            $dead = $damage2 / $HP;
+            echo "<p>dead $dead </p>";
+            $dead = floor($dead);
+
+            $damage2 -= $dead * $HP;
+            echo "<p>damage after: $damage2 </p>";
+
+            echo "<p>dead: $dead </p>";
+            $moarsql = "UPDATE Player_Units SET Num = (Num - $dead) where Name = '$nom' and Unit_ID = $ID";
+
+            if ($conn->query($moarsql) === TRUE) {
+                
+
+            } else {
+                echo "Error updating record: " . $conn->error;
+            }
+
+
+
         }
 
     }
@@ -83,7 +126,44 @@ if ($result->num_rows > 0) {
 else {
     echo "0 results";
 }
+$healthy = 0;
 
+$sql = "Select * from Unit,Player_Units where Unit.Unit_ID = Player_Units.Unit_ID and Player_Units.Name = '$attack' and Ran in (Select min(Ran) from Unit,Player_Units where Unit.Unit_ID = Player_Units.Unit_ID and Num > 0 and Player_Units.Name = '$attack')";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["Name"] . " - Name: " . $row["Unit_ID"] . " " . $row["Num"] . "<br>";
+        echo "<p>healthy $healthy </p>";
+        $healthy+= $row["HP"];
+        echo "<p>healthy $healthy </p>";
+        $ID = $row["Unit_ID"];
+
+        echo "<p>healthy $healthy </p>";
+        $healthy *= $row["Num"];
+        echo "<p>healthy $healthy </p>";
+        echo "<p>damage $damage </p>";
+
+
+        if ($damage > $healthy) {
+
+            $sql = "UPDATE Player_Units SET Num = 0 where Unit_ID = $ID and Player_Units.Name = '$attack'";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "<p> they died </p>";
+            } else {
+                echo "Error updating record: " . $conn->error;
+            }
+
+
+
+        }
+
+    }
+}
+else {
+    echo "0 results";
+}
 
 
 if ($strength > $strength2) {

d7920cedd1fa62efee70c201f270be30aa9826b5
Jack <crdavies@hotmail.co.uk>
2018-05-02 10:03:02 +0100

    login commented

--- login_handler.php
+++ login_handler.php
@@ -1,18 +1,25 @@
 
 <?php
+//checks to see if login details are filled
 	if (empty($_POST['name']) || empty($_POST['password'])) {
 		header("Location: login.php?msg=4");
 	}
 	else {
+		// require db connect that does not exist
 		require_once("dbconnect.php");
+		// sets the name and password hashed with md5 wow, so secure!
 		$name=$_POST['name'];
 		$password=md5($_POST['password']);
-        echo "<p> $password </p>";
+		// create the sql to get the user details
 		
 		$sql = "SELECT * FROM Player WHERE Password='".$password."' AND Name='".$name."'";
 		$result = $conn->query($sql);
 		if ($result->num_rows == 1) {
+		    // starts the session!
 			session_start();
+			// one of these is redundant... But they set the session name to the player name
+            // I can't remember which one worked...
+            // I'm pretty sure that there isn't even a player ID anymore, but I'm too scared to delete things now
 			$_SESSION['name']=$name;
 			while($row = $result->fetch_assoc()) {
 				$_SESSION['name']=$row["Name"];
--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 
 <?php
 	session_start();
+	//logs the player out by destroying the session
 	if(session_destroy())
 	{
 		header("Location: login.php");
